2ab4c9bd6937155ace72fcb3f36ad9fbd300140a
Des Dulianto <desdulianto@gmail.com>
2020-08-26 20:44:48 +0700

    2nd anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn is_anagram(sorted_chars: &Vec<char>, other: &str) -> bool {
     let mut other: Vec<char> = other.chars().collect();
@@ -7,20 +8,18 @@ fn is_anagram(sorted_chars: &Vec<char>, other: &str) -> bool {
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut anagrams = HashSet::new();
     let word = word.to_lowercase();
     let mut sorted_chars = word.chars().collect::<Vec<char>>();
     sorted_chars.sort();
 
-    possible_anagrams
-        .iter()
-        .filter(|x| {
-            let other = x.to_lowercase();
-            let other = other.as_str();
-            word != other && is_anagram(&sorted_chars, other)
-        })
-        .for_each(|x| {
-            anagrams.insert(*x);
-        });
-    anagrams
+    HashSet::from_iter(
+        possible_anagrams
+            .iter()
+            .filter(|x| {
+                let other = x.to_lowercase();
+                let other = other.as_str();
+                word != other && is_anagram(&sorted_chars, other)
+            })
+            .cloned(),
+    )
 }
