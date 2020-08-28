517ef90980b891a653dc82d161f0d1fa5c9c1399
Des Dulianto <desdulianto@gmail.com>
2020-08-28 21:13:06 +0700

    use macros for space-age

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,6 +1,18 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+macro_rules! orbital_periods (
+    { $($t:ty => $x:expr),* } => {
+        $(
+            impl Planet for $t {
+                fn orbital_period() -> f64 {
+                    $x
+                }
+            }
+        )*
+    };
+);
+
 #[derive(Debug)]
 pub struct Duration(u64);
 
@@ -29,50 +41,13 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {
-    fn orbital_period() -> f64 {
-        0.2408467
-    }
-}
-
-impl Planet for Venus {
-    fn orbital_period() -> f64 {
-        0.61519726
-    }
-}
-
-impl Planet for Earth {
-    fn orbital_period() -> f64 {
-        1.0
-    }
-}
-
-impl Planet for Mars {
-    fn orbital_period() -> f64 {
-        1.8808158
-    }
-}
-
-impl Planet for Jupiter {
-    fn orbital_period() -> f64 {
-        11.862615
-    }
-}
-
-impl Planet for Saturn {
-    fn orbital_period() -> f64 {
-        29.447498
-    }
-}
-
-impl Planet for Uranus {
-    fn orbital_period() -> f64 {
-        84.016846
-    }
-}
-
-impl Planet for Neptune {
-    fn orbital_period() -> f64 {
-        164.79132
-    }
-}
+orbital_periods!(
+    Mercury => 0.2408467,
+    Venus   => 0.61519726,
+    Earth   => 1.0,
+    Mars    => 1.8808158,
+    Jupiter => 11.862615,
+    Saturn  => 29.447498,
+    Uranus  => 84.016846,
+    Neptune => 164.79132
+);
