feabf376b5c957ce96d2eb33cf396c3180eb2ee5
Des Dulianto <desdulianto@gmail.com>
2020-09-04 18:36:08 +0700

    improve code

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,38 +1,19 @@
-fn clean_str(s: &str) -> String {
-    s.chars()
-        .map(|x| {
-            if x.is_alphanumeric() || x == '\'' {
-                x
-            } else {
-                ' '
-            }
-        })
-        .collect::<String>()
-}
-
-fn split_camel_case(s: &str) -> String {
-    s.char_indices()
-        .map(|(i, c)| {
-            if c.is_uppercase() {
-                match (
-                    s.chars().nth((i as isize - 1) as usize),
-                    s.chars().nth(i + 1),
-                ) {
-                    (Some(p), Some(n)) if p.is_lowercase() && n.is_lowercase() => format!(" {}", c),
-                    _ => format!("{}", c),
-                }
-            } else {
-                format!("{}", c)
-            }
-        })
-        .collect::<String>()
-}
-
 pub fn abbreviate(phrase: &str) -> String {
-    let cleaned_str = clean_str(split_camel_case(phrase).as_str());
+    let cleaned_str = phrase.replace(|c: char| !(c.is_alphanumeric() || c == '\''), " ");
     let words = cleaned_str.split_whitespace().collect::<Vec<&str>>();
     words
         .iter()
-        .map(|s| s.chars().next().unwrap().to_uppercase().to_string())
+        .map(|s| {
+            s.chars()
+                .take(1)
+                .chain(
+                    // camel case
+                    s.chars()
+                        .skip_while(|c| c.is_uppercase()) // skip adjacent upper case
+                        .filter(|c| c.is_uppercase()), // filter all uppercase
+                )
+                .collect::<String>()
+        })
         .collect::<String>()
+        .to_uppercase()
 }
