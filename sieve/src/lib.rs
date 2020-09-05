da6f7aa06b6ec521389c8adf4ecd3636149ac7c2
Des Dulianto <desdulianto@gmail.com>
2020-09-05 15:55:08 +0700

    proper sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,13 +1,12 @@
+use std::collections::HashSet;
+
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut primes: Vec<u64> = Vec::new();
+    let mut non_primes: HashSet<u64> = HashSet::new();
     (2..=upper_bound)
         .filter(|x| {
-            if primes.iter().any(|y| x % y == 0) {
-                false
-            } else {
-                primes.push(*x);
-                true
-            }
+            let is_prime = !non_primes.contains(x);
+            (x+x..=upper_bound).step_by(*x as usize).for_each(|y| { non_primes.insert(y); });
+            is_prime
         })
         .collect()
 }
