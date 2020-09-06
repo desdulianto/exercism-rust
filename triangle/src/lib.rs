10717ef718c4c4ae87002b17777fd41718d3bd93
Des Dulianto <desdulianto@gmail.com>
2020-09-06 12:03:43 +0700

    improve triangle

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,13 +1,11 @@
 pub struct Triangle {
-    a: u64,
-    b: u64,
-    c: u64,
+    sides: [u64; 3],
 }
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
         if Triangle::valid_sides(sides) {
-            Some(Triangle { a: sides[0], b: sides[1], c: sides[2] })
+            Some(Triangle { sides })
         } else {
             None
         }
@@ -15,21 +13,24 @@ impl Triangle {
 
     fn valid_sides(sides: [u64; 3]) -> bool {
         sides.iter().all(|x| *x > 0) && {
-            let mut s = [sides[0], sides[1], sides[2]];
+            let mut s = sides;
             s.sort();
             s.iter().take(2).sum::<u64>() >= s[2]
         }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        self.a == self.b && self.b == self.c
+        let [a, b, c] = self.sides;
+        a == b && b == c
     }
 
     pub fn is_scalene(&self) -> bool {
-        self.a != self.b && self.a != self.c && self.b != self.c
+        let [a, b, c] = self.sides;
+        a != b && a != c && b != c
     }
 
     pub fn is_isosceles(&self) -> bool {
-        self.a == self.b || self.a == self.c || self.b == self.c
+        let [a, b, c] = self.sides;
+        a == b || a == c || b == c
     }
 }
